c51ae62f209eee9489e01f11e02e7ec87ad1cb8f
mich123prem <68922815+mich123prem@users.noreply.github.com>
2022-04-29 19:12:53 +0200

    adding ltr-abi as CSRF trusted origin

--- TTTdrf1/settings.py
+++ TTTdrf1/settings.py
@@ -44,12 +44,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CSRF_TRUSTED_ORIGINS = ['https://ltr-abi.no']
 ROOT_URLCONF = 'TTTdrf1.urls'
 
 TEMPLATES = [
